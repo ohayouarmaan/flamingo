ac28f46f804f953ac345416bd657a5a5b13e6c49
Armaan Gupta <ohayouarmaan@gmail.com>
2024-12-24 22:38:23 +0530

    feat: grouping in a primary expression

--- standard/time.py
+++ standard/time.py
@@ -4,6 +4,8 @@ import time
 class Time(Callable):
     def __init__(self):
         super().__init__()
+        self.arity = 0
 
-    def call(self, *args, **kwargs):
+    def call(self, interpreter, args):
         return time.time()
+
